0606f09847dd26e9d29dde5891e6a4bf11be6d1d
Hiruni Piumika <piumikahiruni22@gmail.com>
2023-02-18 12:34:09 +0530

    update customer serviceImpl

--- BackEnd/src/main/java/lk/easycarRental/service/impl/CustomerServiceImpl.java
+++ BackEnd/src/main/java/lk/easycarRental/service/impl/CustomerServiceImpl.java
@@ -1,10 +1,22 @@
 package lk.easycarRental.service.impl;
 
 import lk.easycarRental.dto.CustomerDTO;
+import lk.easycarRental.entity.Customer;
+import lk.easycarRental.repo.CustomerRepo;
 import lk.easycarRental.service.CustomerService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class CustomerServiceImpl implements CustomerService {
-    public void customerSave(CustomerDTO dto) {
+    @Autowired
+    private CustomerRepo repo;
+    @Autowired
+    private ModelMapper mapper;
+    public void saveCustomer(CustomerDTO dto) {
+        repo.save(mapper.map(dto,Customer.class));
+    }
+
+    public void deleteCustomer(String id) {
 
     }
 }
