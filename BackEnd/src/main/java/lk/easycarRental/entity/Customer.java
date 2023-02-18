934336b171cf5a67f1027b30d8677c3250789dd3
Hiruni Piumika <piumikahiruni22@gmail.com>
2023-02-18 11:26:37 +0530

    add Customer

---
+++ BackEnd/src/main/java/lk/easycarRental/entity/Customer.java
@@ -0,0 +1,28 @@
+package lk.easycarRental.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class Customer {
+
+    @Id
+    private String nic;
+    private String name;
+    private String address;
+    private String password;
+    private String licence;
+}
+
+
+
