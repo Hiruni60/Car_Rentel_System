8efea94a55ac97857c5bc6cd77b3696d03fba608
Hiruni Piumika <piumikahiruni22@gmail.com>
2023-02-18 11:20:39 +0530

    add CustomerServiceimpl

---
+++ BackEnd/src/main/java/lk/easycarRental/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,10 @@
+package lk.easycarRental.service.impl;
+
+import lk.easycarRental.dto.CustomerDTO;
+import lk.easycarRental.service.CustomerService;
+
+public class CustomerServiceImpl implements CustomerService {
+    public void customerSave(CustomerDTO dto) {
+
+    }
+}
