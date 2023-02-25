a6c1c15722c239a0f902a07c0591b3bc655d0836
Hiruni Piumika <piumikahiruni22@gmail.com>
2023-02-25 11:27:38 +0530

    updating Car entity

--- BackEnd/src/main/java/lk/easycarRental/entity/Car.java
+++ BackEnd/src/main/java/lk/easycarRental/entity/Car.java
@@ -1,4 +1,30 @@
 package lk.easycarRental.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class Car {
+    @Id
+    private String vehicalNo;
+    private String brand;
+    private String type;
+    private int noOfPassenger;
+    private String transmissionType;
+    private String fuelType;
+    private BigDecimal monthlyPrice;
+    private BigDecimal dailyPrice;
+    private BigDecimal priceForExtraKM;
+    private String colour;
+    private boolean available;
 }
