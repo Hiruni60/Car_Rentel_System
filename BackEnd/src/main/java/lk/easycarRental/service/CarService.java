1ae5fd940aeb676679ce8bcf2e7834ef954104ca
Hiruni Piumika <piumikahiruni22@gmail.com>
2023-02-25 11:26:40 +0530

    updating CarService

--- BackEnd/src/main/java/lk/easycarRental/service/CarService.java
+++ BackEnd/src/main/java/lk/easycarRental/service/CarService.java
@@ -1,4 +1,16 @@
 package lk.easycarRental.service;
 
+import lk.easycarRental.dto.CarDTO;
+import lk.easycarRental.dto.DriverDTO;
+
+import java.util.ArrayList;
+
 public interface CarService {
+    void saveCar(CarDTO dto);
+
+    void deleteCar(String id);
+
+    void updateCar(CarDTO dto);
+
+    ArrayList<CarDTO> getAllCars();
 }
