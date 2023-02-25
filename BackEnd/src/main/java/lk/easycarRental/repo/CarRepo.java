7f043434ddf5db755d46eed87dffbd0ac3cdf3a7
Hiruni Piumika <piumikahiruni22@gmail.com>
2023-02-25 11:26:51 +0530

    updating CarRepo

--- BackEnd/src/main/java/lk/easycarRental/repo/CarRepo.java
+++ BackEnd/src/main/java/lk/easycarRental/repo/CarRepo.java
@@ -1,4 +1,7 @@
 package lk.easycarRental.repo;
 
-public interface CarRepo {
+import lk.easycarRental.entity.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarRepo extends JpaRepository<Car,String> {
 }
