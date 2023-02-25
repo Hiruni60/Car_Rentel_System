d9e46425f9dde45b02d2a07d0cad3ee900bf8ee7
Hiruni Piumika <piumikahiruni22@gmail.com>
2023-02-25 11:27:02 +0530

    updating CarDto

--- BackEnd/src/main/java/lk/easycarRental/dto/CarDTO.java
+++ BackEnd/src/main/java/lk/easycarRental/dto/CarDTO.java
@@ -1,4 +1,27 @@
 package lk.easycarRental.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class CarDTO {
+    private String vehicalNo;
+    private String brand;
+    private String type;
+    private int noOfPassenger;
+    private String transmissionType;
+    private String fuelType;
+    private BigDecimal monthlyPrice;
+    private BigDecimal dailyPrice;
+    private BigDecimal priceForExtraKM;
+    private String colour;
+    private boolean available;
 }
