ba6f8b62e4732a1e454869658add766c1e6a978e
Hiruni Piumika <piumikahiruni22@gmail.com>
2023-02-25 11:24:33 +0530

    updating webAppConfig

--- BackEnd/src/main/java/lk/easycarRental/config/WebAppConfig.java
+++ BackEnd/src/main/java/lk/easycarRental/config/WebAppConfig.java
@@ -1,6 +1,7 @@
 package lk.easycarRental.config;
 
 import lk.easycarRental.advisor.AppWideExceptionHandler;
+import lk.easycarRental.controller.CarController;
 import lk.easycarRental.controller.CustomerController;
 import lk.easycarRental.controller.DriverController;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
     @Configuration
     @EnableWebMvc
-    @ComponentScan(basePackageClasses = {CustomerController.class, AppWideExceptionHandler.class, DriverController.class})
+    @ComponentScan(basePackageClasses = {CustomerController.class, AppWideExceptionHandler.class, DriverController.class, CarController.class})
     public class WebAppConfig {
 
 
