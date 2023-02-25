df627e412b70d46c4c5fe243f069fc8af811c417
Hiruni Piumika <piumikahiruni22@gmail.com>
2023-02-25 11:26:28 +0530

    updating CarServiceImpl

--- BackEnd/src/main/java/lk/easycarRental/service/impl/CarServiceImpl.java
+++ BackEnd/src/main/java/lk/easycarRental/service/impl/CarServiceImpl.java
@@ -1,4 +1,50 @@
 package lk.easycarRental.service.impl;
 
-public class CarServiceImpl {
+import lk.easycarRental.dto.CarDTO;
+
+import lk.easycarRental.entity.Car;
+
+import lk.easycarRental.repo.CarRepo;
+import lk.easycarRental.service.CarService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+
+@Service
+@Transactional
+public class CarServiceImpl implements CarService {
+    @Autowired
+    private CarRepo repo;
+    @Autowired
+    private ModelMapper mapper;
+
+    public void saveCar(CarDTO dto){
+        repo.save(mapper.map(dto, Car.class));
+    }
+    @Override
+    public void deleteCar(String id) {
+        if (!repo.existsById(id)){
+            throw new RuntimeException("Wrong nic..Please Check the nic");
+        }
+        repo.deleteById(id);
+    }
+
+    @Override
+    public void updateCar(CarDTO dto) {
+        if (!repo.existsById(dto.getVehicalNo())){
+            throw new RuntimeException("Wrong nic..Please Check the nic");
+        }
+        repo.save(mapper.map(dto, Car.class));
+    }
+
+
+    @Override
+    public ArrayList<CarDTO> getAllCars() {
+        return mapper.map(repo.findAll(),new TypeToken<ArrayList<CarDTO>>(){}.getType());
+    }
+
 }
